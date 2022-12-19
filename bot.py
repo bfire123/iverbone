aff4e82b0f781e0908d3319d77307b60c4d4bcfb
bfire123 <106290477+bfire123@users.noreply.github.com>
2022-12-19 07:03:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,7 +43,6 @@ class Bot(Client):
         logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
         logging.info(LOG_STR)
   
-      
     async def stop(self, *args):
         await super().stop()
         logging.info("Bot stopped. Bye.")
